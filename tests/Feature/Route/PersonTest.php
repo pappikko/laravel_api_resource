017e3f40caa73e40f46dcf15438c0926b6ac9825
sukehiro <sukehiro@3films.com>
2019-07-03 01:15:05 +0900

    PersonTestを実装

--- tests/Feature/Route/PersonTest.php
+++ tests/Feature/Route/PersonTest.php
@@ -32,9 +32,9 @@ class PersonTest extends TestCase
         $this->personMock
             ->shouldReceive('delete')
             ->once()
-            ->andReturn(1);
+            ->andReturn(3);
         $this->app->instance('App\Models\Person', $this->personMock);
-        $response = $this->json('DELETE', 'api/person/1');
+        $response = $this->json('DELETE','api/person/3');
         $response->assertStatus(200);
     }
 
@@ -51,26 +51,28 @@ class PersonTest extends TestCase
             'weight' => '60',
             'height' => '1.72'
         ]);
-        $response->assertStatus(200);
+        $response->assertStatus(201);
     }
 
     public function testPersonView()
     {
-        $response = $this->json('GET', 'api/person/1');
+        $response = $this->json('GET', 'api/person/3');
         $response->assertStatus(200);
     }
 
     public function testPersonUpdate()
     {
-        $response = $this->json('PUT', 'api/person/1', [
-            'name' => 'Johnson'
+        $response = $this->json('PUT', 'api/person/3', [
+            'name' => 'Johnson',
+            'height' => '122',
+            'weight' => '44',
         ]);
         $response->assertStatus(200);
     }
 
     // public function testPersonDelete()
     // {
-    //     $response = $this->json('DELETE', 'api/person/1');
+    //     $response = $this->json('DELETE', 'api/person/3');
     //     $response->assertStatus(200);
     // }
 }
